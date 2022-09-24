cd562fadd640a60e0c4dcea021c69a5fbce6e12d
Vqid <57548475+Vqid@users.noreply.github.com>
2022-09-23 20:50:44 -0500

    Chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+use colored::*;
+
+fn main(){
+    println!("{}", "Guess the number!".blue());
+
+    let _secret_number = rand::thread_rng().gen_range(1..=100);
+
+    // println!("The secret number is: {}", _secret_number);
+
+    loop {
+        println!("{}", "Please input your guess. ".blue());
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&_secret_number){
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Greater => println!("{}", "Too big!".red()),
+            Ordering::Equal => {
+                println!("{}", "You win!".green());
+                break;
+            },
+        }
+    }
+
 }
